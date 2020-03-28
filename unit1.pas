9ae9bcfb95612d6fb12e9dd20405e5b0749328cf
pazworld <paz@moudamepo.net>
2020-03-29 01:06:11 +0900

    initial

---
+++ unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    ListBox1: TListBox;
+    Memo1: TMemo;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
